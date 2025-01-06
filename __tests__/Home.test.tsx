d066113a4fe1b665bc4d972c93d1a33b2349243f
nattapos <a0ffis.ntp@gmail.com>
2025-01-06 16:10:56 +0700

    feat: init nextjs and jest testing on index

---
+++ __tests__/Home.test.tsx
@@ -0,0 +1,38 @@
+import Home from "@/app/page";
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+
+describe("Home Page", () => {
+  describe("Init render ~~~", () => {
+    it("renders a logo", () => {
+      render(<Home />);
+      const ele = screen.getByTestId("logo-nextjs");
+      expect(ele).toBeInTheDocument();
+    });
+
+    it("renders a deploy", () => {
+      render(<Home />);
+      const ele = screen.getByText(/Deploy now/i);
+      expect(ele).toBeInTheDocument();
+    });
+
+    it("renders a read our docs", () => {
+      render(<Home />);
+      const ele = screen.getByText(/Read our docs/i);
+      expect(ele).toBeInTheDocument();
+    });
+
+    it("renders the correct link", () => {
+      render(<Home />);
+
+      // Find the link element
+      const link = screen.getByText(/Deploy now/i);
+
+      // Assert the href attribute
+      expect(link).toHaveAttribute(
+        "href",
+        "https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app",
+      );
+    });
+  });
+});
